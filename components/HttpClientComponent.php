6bf52f273f135696388dda39a81a6712751eca94
Charles R. Portwood II <charlesportwoodii@erianna.com>
2018-05-07 14:34:03 -0500

    Using fullURL which is parsed instead of getUrl which can be an array

--- components/HttpClientComponent.php
+++ components/HttpClientComponent.php
@@ -53,7 +53,7 @@ final class HttpClientComponent extends BaseObject
 
         $this->client->on(Client::EVENT_BEFORE_SEND, function(RequestEvent $e) {
             Yii::info([
-                'message' => sprintf('Sending HTTP request [%s] %s', $e->request->getMethod(), $e->request->getUrl()),
+                'message' => sprintf('Sending HTTP request [%s] %s', $e->request->getMethod(), $e->request->getFullUrl()),
                 'data' => $e->request->getData(),
                 'user_id' => Yii::$app->user->id ?? null
             ], 'httpclient');
@@ -61,7 +61,7 @@ final class HttpClientComponent extends BaseObject
 
         $this->client->on(Client::EVENT_AFTER_SEND, function(RequestEvent $e) {
             Yii::info([
-                'message' => sprintf('Recieved HTTP response HTTP [%s] | [%s] %s', $e->response->getStatusCode(), $e->request->getMethod(), $e->request->getUrl()),
+                'message' => sprintf('Recieved HTTP response HTTP [%s] | [%s] %s', $e->response->getStatusCode(), $e->request->getMethod(), $e->request->getFullUrl()),
                 'data' => (function () use ($e) {
                     $content = $e->response->getContent();
                     if (preg_match('~[^\x20-\x7E\t\r\n]~', $content) > 0) {
